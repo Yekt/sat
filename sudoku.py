b58da1c85d30d2ff95b3a1181c0a85e753f08430
Lukas <l-sauter@web.de>
2018-12-05 12:21:28 +0100

    Multiprocessing cuts down time to a quarter

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,11 @@
 import math
+import time
 import subprocess
+from multiprocessing import Pool
 
 
 def main():
+    start_time = time.time()
 
     ### READING INPUT FILE ###
     with open ("bsp-sudoku-input.txt", "r") as input:
@@ -24,7 +27,6 @@ def main():
     n = len(sudoku)
     n_sub = int(math.sqrt(n))
     n_len = len(str(n))
-    #print('Sudoku size is %sx%s' % (n, n))
 
     # fill grid
     grid = [["-" for x in range(n)] for y in range(n)]
@@ -40,128 +42,39 @@ def main():
     nv = int(str(n)*3)
     nc = 0
 
-    # EACH ENTRY
+    pool = Pool(processes=5)
 
-    # AT LEAST ONE NUMBER
-    c1 = ''
-    for x in range(1, n+1):
-        for y in range(1, n+1):
-            for z in range(1, n+1):
-                c1 += ctv(x, y, z, n_len) + ' '
-            c1 += "0\n"
-            nc += 1
-    print('EACH ENTRY, at least one number: done')
+    p1 = pool.map_async(process1, [(n,n_sub,n_len),])
+    p2 = pool.map_async(process2, [(n,n_sub,n_len),])
+    p3 = pool.map_async(process3, [(n,n_sub,n_len),])
+    p4 = pool.map_async(process4, [(n,n_sub,n_len),])
+    p5 = pool.map_async(process5, [(n,n_sub,n_len),])
 
-    # AT MOST ONE NUMBER
-    c2 = ''
-    for x in range(1, n+1):
-        for y in range(1, n+1):
-            for z in range(1, n):
-                for i in range(z+1, n+1):
-                    c2 += "-" + ctv(x, y, z, n_len) + " " + "-" + ctv(x, y, i, n_len) + " 0\n"
-                    nc += 1
-        print('row done: '+ str(x))
-    print('EACH ENTRY, at most one number: done')
+    pool.close()
+    pool.join()
 
-    # EACH ROW
-
-    # EACH NUMBER AT MOST ONCE
-    c3 = ''
-    for x in range(1, n + 1):
-        for z in range(1, n+1):
-            for y in range(1, n):
-                for i in range(y + 1, n + 1):
-                    c3 += "-" + ctv(x, y, z, n_len) + " " + "-" + ctv(x, i, z, n_len) + " 0\n"
-                    nc += 1
-        print('row done: '+ str(x))
-    print('EACH ROW, each number at most once: done')
-
-    # EACH NUMBER AT LEAST ONCE
-    c4 = ''
-    for x in range(1, n+1):
-        for z in range(1, n+1):
-            for y in range(1, n+1):
-                c4 += ctv(x, y, z, n_len) + ' '
-            c4 += "0\n"
-            nc += 1
-        print('row done: '+ str(x))
-    print('EACH ROW, each number at least once: done')
-
-
-    # EACH COLUMN
-
-    # EACH NUMBER AT MOST ONCE
-    c5 = ''
-    for y in range(1, n+1):
-        for z in range(1, n+1):
-            for x in range(1, n):
-                for i in range(x+1, n+1):
-                    c5 += "-" + ctv(x, y, z, n_len) + " " + "-" + ctv(i, y, z, n_len) + " 0\n"
-                    nc += 1
-        print('column done: '+ str(y))
-    print('EACH COLUMN, each number at most once: done')
-
-    # EACH NUMBER AT LEAST ONCE
-    c6 = ''
-    for y in range(1, n+1):
-        for z in range(1, n+1):
-            for x in range(1, n+1):
-                c6 += ctv(x, y, z, n_len) + ' '
-            c6 += "0\n"
-            nc += 1
-        print('column done: '+ str(y))
-    print('EACH COLUMN, each number at least once: done')
-
-    # SUB-GRID
-
-    # EACH NUMBER AT MOST ONCE
-    c7 = ''
-    for z in range(1, n+1):
-        for i in range(0, n_sub):
-            for j in range(0, n_sub):
-                for x in range(1, n_sub+1):
-                    for y in range(1, n_sub+1):
-                        for k in range(y+1, n_sub+1):
-                            c7 += "-" + ctv((n_sub*i + x), (n_sub*j + y), z, n_len) + " -" + ctv((n_sub*i+x), (n_sub*j+k), z, n_len) + " 0\n"
-                            nc += 1
-        print('number done: '+ str(z))
-    print('SUB-GRID, each number at most once (1): done')
-
-    c8 = ''
-    for z in range(1, n+1):
-        for i in range(0, n_sub):
-            for j in range(0, n_sub):
-                for x in range(1, n_sub+1):
-                    for y in range(1, n_sub+1):
-                        for k in range(x+1, n_sub+1):
-                            for l in range(1, n_sub+1):
-                                c8 += "-" + ctv((n_sub*i + x), (n_sub*j + y), z, n_len) + " -" + ctv((n_sub*i+k), (n_sub*j+l), z, n_len) + " 0\n"
-                                nc += 1
-        print('number done: '+ str(z))
-    print('SUB-GRID, each number at most once (2): done')
-
-    # EACH NUMBER AT LEAST ONCE
-    c9 = ''
-    for i in range(1, n_sub):
-        for j in range(1, n_sub):
-            for x in range(1, n_sub+1):
-                for y in range(1, n_sub+1):
-                    for z in range(1, n+1):
-                        c9 += ctv((n_sub*i+x), (n_sub*j+y), z, n_len) + " "
-                    c9 += "0\n"
-                    nc += 1
-        print('number done: '+ str(i))
-    print('SUB-GRID, each number at least once: done')
-
-    # SET EXISTING NUMBERS
     c0 = ''
     for x in range(n):
         for y in range(n):
             if grid[x][y] != "-":
                 c0 += ctv(x+1, y+1, grid[x][y], n_len) + " 0\n"
                 nc += 1
-        print('row done: '+str(x))
-    print('SET EXISTING NUMBERS: done')
+    #print('SET EXISTING NUMBERS')
+
+    c1 = p1.get()[0][0]
+    c4 = p1.get()[0][1]
+    c6 = p1.get()[0][2]
+    c7 = p1.get()[0][3]
+    c9 = p1.get()[0][4]
+    nc+= p1.get()[0][5]
+    c2 = p2.get()[0][0]
+    nc+= p2.get()[0][1]
+    c3 = p3.get()[0][0]
+    nc+= p3.get()[0][1]
+    c8 = p4.get()[0][0]
+    nc+= p4.get()[0][1]
+    c5 = p5.get()[0][0]
+    nc+= p5.get()[0][1]
 
     f = open('sudo.cnf', 'w')
     f.write("p cnf " + str(nv) + " " + str(nc) + "\n")
@@ -179,15 +92,12 @@ def main():
     print("cnf written")
 
 
-
     ### RUN RISS AND CATCH stdout ###
     riss = subprocess.Popen("./riss_505/bin/riss sudo.cnf", stdout=subprocess.PIPE, shell=True)
     # might be optimizable
     (output, error) = riss.communicate()
     status = riss.wait()
-    print(output)
-    #print('RISS Ccompleted')
-
+    print('RISS completed')
 
 
     ### COMPLETE GRID FROM RISS OUTPUT ###
@@ -208,34 +118,170 @@ def main():
     print('grid completed')
 
 
-
-
     ### WRITING OUTPUT TXT ###
+    out = ''
     # TODO missing information:
         #experiment: generator (Time: 0.001419 s)
         #number of tasks: 1
         #task: 1
         #puzzle size: 3x3
-    txt = open('output.txt', 'w')
-    txt.write(spacer(n_sub, n_len))
+    out += spacer(n_sub, n_len)
     x = 0
     for block_vertical in range(0,n_sub):
         for row in range(0,n_sub):
-            txt.write('| ')
+            out += '| '
             y = 0
             for block_horizontal in range(0,n_sub):
                 for col in range(0,n_sub):
                     z = grid[x][y]
-                    txt.write(' ' * (n_len - len(z)))
-                    txt.write(z)
-                    txt.write(' ')
+                    out += ' ' * (n_len - len(z))
+                    out += z + ' '
                     y += 1
-                txt.write('| ')
-            txt.write('\n')
+                out += '| '
+            out += '\n'
             x += 1
-        txt.write(spacer(n_sub, n_len))
+        out += spacer(n_sub, n_len)
+    txt = open('output.txt', 'w')
+    txt.write(out)
     txt.close()
     print('done')
+    print('time needed: '+ str(time.time()-start_time))
+
+
+
+
+### splitting the work load in 5 processes for multi core use ###
+def process1(triple):
+    print('->PROCESS 1 started')
+    t = time.time()
+    nc = 0
+    n = triple[0]
+    n_sub = triple[1]
+    n_len = triple[2]
+    c1 = ''
+    for x in range(1, n+1):
+        for y in range(1, n+1):
+            for z in range(1, n+1):
+                c1 += ctv(x, y, z, n_len) + ' '
+            c1 += "0\n"
+            nc += 1
+    #print('EACH ENTRY, at least one number')
+    c4 = ''
+    for x in range(1, n+1):
+        for z in range(1, n+1):
+            for y in range(1, n+1):
+                c4 += ctv(x, y, z, n_len) + ' '
+            c4 += "0\n"
+            nc += 1
+    #print('EACH ROW, each number at least once')
+    c6 = ''
+    for y in range(1, n+1):
+        for z in range(1, n+1):
+            for x in range(1, n+1):
+                c6 += ctv(x, y, z, n_len) + ' '
+            c6 += "0\n"
+            nc += 1
+    #print('EACH COLUMN, each number at least once')
+    c7 = ''
+    for z in range(1, n+1):
+        for i in range(0, n_sub):
+            for j in range(0, n_sub):
+                for x in range(1, n_sub+1):
+                    for y in range(1, n_sub+1):
+                        for k in range(y+1, n_sub+1):
+                            c7 += "-" + ctv((n_sub*i + x), (n_sub*j + y), z, n_len) + " -" + ctv((n_sub*i+x), (n_sub*j+k), z, n_len) + " 0\n"
+                            nc += 1
+    #print('SUB-GRID, each number at most once (1)')
+    c9 = ''
+    for i in range(1, n_sub):
+        for j in range(1, n_sub):
+            for x in range(1, n_sub+1):
+                for y in range(1, n_sub+1):
+                    for z in range(1, n+1):
+                        c9 += ctv((n_sub*i+x), (n_sub*j+y), z, n_len) + " "
+                    c9 += "0\n"
+                    nc += 1
+    #print('SUB-GRID, each number at least once')
+    print('->PROCESS 1 finished, total time: ' + str(time.time() - t))
+    return (c1,c4,c6,c7,c9,nc)
+
+
+def process2(triple):
+    print('->PROCESS 2 started')
+    t = time.time()
+    nc = 0
+    n = triple[0]
+    n_sub = triple[1]
+    n_len = triple[2]
+    c2 = ''
+    for x in range(1, n+1):
+        for y in range(1, n+1):
+            for z in range(1, n):
+                for i in range(z+1, n+1):
+                    c2 += "-" + ctv(x, y, z, n_len) + " " + "-" + ctv(x, y, i, n_len) + " 0\n"
+                    nc += 1
+    #print('EACH ENTRY, at most one number')
+    print('->PROCESS 2 finished, total time: ' + str(time.time() - t))
+    return (c2,nc)
+
+
+def process3(triple):
+    print('->PROCESS 3 started')
+    t = time.time()
+    nc = 0
+    n = triple[0]
+    n_len = triple[2]
+    c3 = ''
+    for x in range(1, n + 1):
+        for z in range(1, n+1):
+            for y in range(1, n):
+                for i in range(y + 1, n + 1):
+                    c3 += "-" + ctv(x, y, z, n_len) + " " + "-" + ctv(x, i, z, n_len) + " 0\n"
+                    nc += 1
+    #print('EACH ROW, each number at most once')
+    print('->PROCESS 3 finished, total time: ' + str(time.time() - t))
+    return (c3,nc)
+
+
+def process4(triple):
+    print('->PROCESS 4 started')
+    t = time.time()
+    nc = 0
+    n = triple[0]
+    n_sub = triple[1]
+    n_len = triple[2]
+    c8 = ''
+    for z in range(1, n+1):
+        for i in range(0, n_sub):
+            for j in range(0, n_sub):
+                for x in range(1, n_sub+1):
+                    for y in range(1, n_sub+1):
+                        for k in range(x+1, n_sub+1):
+                            for l in range(1, n_sub+1):
+                                c8 += "-" + ctv((n_sub*i + x), (n_sub*j + y), z, n_len) + " -" + ctv((n_sub*i+k), (n_sub*j+l), z, n_len) + " 0\n"
+                                nc += 1
+    #print('SUB-GRID, each number at most once (2)')
+    print('->PROCESS 4 finished, total time: ' + str(time.time() - t))
+    return (c8,nc)
+
+
+def process5(triple):
+    print('->PROCESS 5 started')
+    t = time.time()
+    nc = 0
+    n = triple[0]
+    n_len = triple[2]
+    c5 = ''
+    for y in range(1, n+1):
+        for z in range(1, n+1):
+            for x in range(1, n):
+                for i in range(x+1, n+1):
+                    c5 += "-" + ctv(x, y, z, n_len) + " " + "-" + ctv(i, y, z, n_len) + " 0\n"
+                    nc += 1
+    #print('EACH COLUMN, each number at most once')
+    print('->PROCESS 5 finished, total time: ' + str(time.time() - t))
+    return (c5,nc)
+
 
 
 
@@ -253,20 +299,6 @@ def spacer(root, len):
             result += '-'
     result += '+\n'
     return result
-
-def print_grid(grid):
-    for x in grid:
-        for y in x:
-            end= ' '
-            print(y, end)
-        print()
-
-
-def print_file():
-    print("===============\nFile CONTENT")
-    f = open('sudo.cnf', 'r')
-    print(f.read())
-    f.close()
 
 
 if __name__ == '__main__':
